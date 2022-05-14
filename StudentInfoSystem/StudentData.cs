399f6cefe3ec764fc40ae330822f2eea17fc2af8
Joro1111 <102698049+Joro1111@users.noreply.github.com>
2022-05-15 00:00:39 +0300

    qwerty

---
+++ StudentInfoSystem/StudentData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoSystem
+{
+    public class StudentData
+    {
+        private List<Student> TestStudents;
+
+        public StudentData()
+        {
+            TestStudents = new List<Student>();
+            TestStudents.Add(exampleStudent());
+        }
+
+        public List<Student> getStudents()
+        {
+            return this.TestStudents;
+        }
+
+        private void setStudents(List<Student> students)
+        {
+            this.TestStudents = students;
+        }
+
+        private Student exampleStudent()
+        {
+            Student student = new Student("Иван", "Петров", "Иванов", "ФКСТ", "КСИ", "Бакалавър", "Заверил",
+                "121219456", 3, 9, 30);
+
+            return student;
+        }
+
+        public Student IsThereStudent(string facNum)
+        {
+            StudentInfoContext context = new StudentInfoContext();
+
+            Student result = (from st in context.Students
+                              where st.facultyNumber == facNum
+                              select st).FirstOrDefault();
+
+            return result;
+        }
+    }
+}
